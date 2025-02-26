0ede2f6bf6b38e00c31f68160a6f3a6d4b06d39e
VISHAL MEHTA <101457076+vvmmm@users.noreply.github.com>
2025-02-27 02:53:22 +0530

    Update Travel_Agency,py
    Update Travel_Agency,py

--- Travel_Agency.py
+++ Travel_Agency.py
@@ -2,7 +2,7 @@
 from langchain_core.prompts import ChatPromptTemplate
 
 chat_prompt_template= ChatPromptTemplate(
-    messages=[("system","You act As a helpful AI Assistant for a travel agency. which help to find the flight, railway or bus bookings from source to destination. also give list of all available plan for travel in the form of table by mentioning all required details about the trip cost and facilities and etc."),
+    messages=[("system","You act As a helpful AI Assistant for a travel agency. which help to find the flight, railway or bus bookings from source to destination. also give list of all available plan for travel in the form of table by mentioning all required details about the trip cost and facilities in detailed manner and etc."),
               ("human","Book a flight from {source} to {destination}.On date {date} .Have {passengers} no. of passengers. Dont ask any more informations. just give list of all available flights,railways and bus.")],
     partial_variables={"source":"ABD","destination":"HYB","passengers":1}
 )
@@ -28,14 +28,17 @@ chain = chat_prompt_template | chat_model | parser
 
 import streamlit as st
 
-st.title(":tophat: Mehta Travel Planner ~~")
-source=st.text_area(label="Source:",placeholder="Enter Your Source...")
-destination=st.text_area(label="Destination:",placeholder="Enter Your Destination...")
-date=st.text_area(label="Date:",placeholder="Enter Your Journey Destination...")
-passengers=st.text_area(label="No. Of Passengers:",placeholder="Enter No. Of Passengers...")
+st.title(":tophat: AI Based Travel Planner")
+source=st.text_input(label=":earth_asia:Source:",placeholder="Enter Your Source...")
+destination=st.text_input(label=":earth_asia:Destination:",placeholder="Enter Your Destination...")
+date=st.date_input(label=":calendar:Date:",value=None )
+passengers=st.number_input(label=":boy:No. Of Passengers:",min_value=1, max_value=10, value=1, step=1,placeholder="Enter No. Of Passengers...")
 
 btn_click=st.button("Find A Trip Plan Availibility")
 raw_input={"source":source,"destination":destination,"date":date,"passengers":passengers}
 if btn_click==True:
-    st.write(chain.invoke(raw_input))
+    if source==True and destination==True and date==True and passengers==True:
+        st.write(chain.invoke(raw_input))
+    else:
+        st.write("Please fullfill all the above required details to get journey plan....")
     
